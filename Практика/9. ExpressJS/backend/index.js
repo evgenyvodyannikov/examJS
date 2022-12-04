3381975b274ec3a0034a60947c1e9d6289505f2e
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-05 01:08:34 +0500

    Добавил решения задач

--- Практика/9. ExpressJS/backend/index.js
+++ Практика/9. ExpressJS/backend/index.js
@@ -1,17 +1,21 @@
-
 // Импортируем пакет cors
 import cors from 'cors';
 
 // Импортируем пакет express 
 import express from 'express';
 
+import bodyParser from 'body-parser';
+
 // Создаём приложение
 const app = express();
 // Назначаем порт
 const port = 4444;
- 
-// Используем пакет CORS
+
+// Используем пакет CORS (без него не выполнятся запросы)
 app.use(cors());
+// Используем пакет bodyParser (без него нельзя будет получить body в запросе)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 // Отслеживаем get запрос на сервер
 app.get('/', (req, res) => {
@@ -21,13 +25,45 @@ app.get('/', (req, res) => {
 
 // Отслеживаем get запрос на сервер
 app.get('/users', (req, res) => {
-    
+
   // Определяем тело ответа 
-    res.json({
-      name: 'Anton',
-      location: 'NY',
-    });
-    
+  res.json({
+    name: 'Anton',
+    location: 'NY',
+  });
+
+});
+
+let posts = [];
+
+// Отслеживаем post запрос на сервер
+app.post('/posts', (req, res) => {
+
+  // Если данные не отправлены возращаем код 400
+  if (!req.body) return res.sendStatus(400);
+
+  try {
+
+    // Парсим данные в JSON
+    let post = req.body;
+    // Добавляем в массив
+    posts.push(post);
+    // Отправляем добавленный пост в качестве ответа
+    res.send(post); // (не будет ответа от сервера если не написать)
+  }
+  catch {
+    // В случае ошибки отправляем код 500
+    res.sendStatus(500);
+  }
+
+});
+
+// Отслеживаем get запрос на сервер
+app.get('/posts', (req, res) => {
+
+  // Определяем тело ответа 
+  res.json(posts);
+
 });
 
 // Назначаем слушателя
--- Практика/9. ExpressJS/frontend/script.js
+++ Практика/9. ExpressJS/frontend/script.js
@@ -1,5 +1,24 @@
-const sendRequest = () => {
-    fetch('http://localhost:4444/users')
+const getPosts = () => {
+    fetch('http://localhost:4444/posts')
     .then(response => response.json())
     .then(data => console.log(data));
+};
+
+const addPost = () => {
+
+    let title = document.getElementById('title').value;
+    let text = document.getElementById('text').value;
+
+    fetch('http://localhost:4444/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: title,
+            text: text,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 };   
